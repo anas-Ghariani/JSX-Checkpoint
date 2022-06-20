05011dbf2cd9e32f9b817aa11dff13af3a352d5c
anas-Ghariani <anasnabilghariani@gmail.com>
2022-06-20 13:24:45 +0300

    anas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./style.css";
+import imageInSrc from "./imageInSrc.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 className="title red">Your name here</h1>
+
+        <br />
+
+        <img src={imageInSrc} />
+
+        <br />
+
+        <img src="./imageInPublic.jpg" />
+      </div>
+
+      <iframe
+        width="956"
+        height="538"
+        src="https://www.youtube.com/embed/MQj-tZEOZko"
+        title="موسيقى هادئة، موسيقا مع مناظر طبيعية خلابة"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
     </div>
   );
 }
